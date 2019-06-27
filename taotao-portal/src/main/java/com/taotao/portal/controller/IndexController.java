71efa5395d5818c61a7287f8ab3c25e597052d9b
nigaknight <1275647040@qq.com>
2019-06-27 11:15:11 +0800

    httpclient的post测试

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.taotao.portal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -9,6 +11,18 @@ public class IndexController {
 	@RequestMapping("/index")
 	public String showIndex() throws Exception {
 		return "index";
+	}
+	
+	@RequestMapping(value = "/httpclient/post", method =RequestMethod.POST)
+	@ResponseBody
+	public String testPost() {
+		return "ok";
+	}
+	
+	@RequestMapping(value = "/httpclient/post2", method =RequestMethod.POST)
+	@ResponseBody
+	public String testPost2(String username, String password) {
+		return "username:"+username+"\t"+"password:"+password;
 	}
 	
 }
--- taotao-portal/src/test/java/com/taotao/httpClient/HttpClientTest.java
+++ taotao-portal/src/test/java/com/taotao/httpClient/HttpClientTest.java
@@ -3,14 +3,21 @@ package com.taotao.httpClient;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
@@ -34,13 +41,13 @@ public class HttpClientTest {
 		
 	}
 	
-	@Test
+
 	public void doGetWithParam() throws ClientProtocolException, IOException, URISyntaxException {
 		// 创建一个httpClient对象
 		CloseableHttpClient client=HttpClients.createDefault();
 		// 创建一个uri对象
-		URIBuilder builder=new URIBuilder("http://www.sogou.com/web");
-		builder.addParameter("query", "许嵩");
+		URIBuilder builder=new URIBuilder("http://www.baidu.com/baidu");
+		builder.addParameter("wd", "许嵩");
 		// 创建一个Get对象
 		HttpGet get=new HttpGet(builder.build());
 		// 执行请求
@@ -54,4 +61,46 @@ public class HttpClientTest {
 		response.close();
 		client.close();
 	}
+	
+
+	public void doPost() throws Exception {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+	
+		//创建一个post对象
+		HttpPost post = new HttpPost("http://localhost:8082/httpclient/post.html");
+		//执行post请求
+		CloseableHttpResponse response = httpClient.execute(post);
+		String string = EntityUtils.toString(response.getEntity());
+		System.out.println(string);
+		response.close();
+		httpClient.close();
+		
+	}
+	
+
+	@Test
+	public void doPostWithParam() throws Exception{
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		
+		//创建一个post对象
+		HttpPost post = new HttpPost("http://localhost:8082/httpclient/post2.html");
+		//创建一个Entity。模拟一个表单
+		List<NameValuePair> kvList = new ArrayList<>();
+		kvList.add(new BasicNameValuePair("username", "zhangsan"));
+		kvList.add(new BasicNameValuePair("password", "123"));
+		
+		//包装成一个Entity对象
+		StringEntity entity = new UrlEncodedFormEntity(kvList, "utf-8");
+		//设置请求的内容
+		post.setEntity(entity);
+		
+		//执行post请求
+		CloseableHttpResponse response = httpClient.execute(post);
+		String string = EntityUtils.toString(response.getEntity());
+		System.out.println(string);
+		response.close();
+		httpClient.close();
+	}
+
+
 }
