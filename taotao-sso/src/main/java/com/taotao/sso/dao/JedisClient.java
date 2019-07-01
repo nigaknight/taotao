c49fdaf135ed73dbe3b7e947d8aacf9db7e78242
nigaknight <1275647040@qq.com>
2019-07-01 15:51:04 +0800

    单点登录系统搭建

---
+++ taotao-sso/src/main/java/com/taotao/sso/dao/JedisClient.java
@@ -0,0 +1,13 @@
+package com.taotao.sso.dao;
+
+public interface JedisClient {
+	public String get(String key);
+	public String set(String key1, String value);
+	public long hset(String hkey, String key, String value);
+	public String hget(String hkey, String key);
+	public long incr(String key);
+	public long expire(String key, int seconds);
+	public long ttl(String key);
+	long hdel(String hkey, String key);
+
+}
---
+++ taotao-sso/src/main/java/com/taotao/sso/dao/JedisClientImpl.java
@@ -0,0 +1,76 @@
+package com.taotao.sso.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class JedisClientImpl implements JedisClient {
+	@Autowired
+	JedisPool jedisPool;
+
+	@Override
+	public String get(String key) {
+		Jedis jedis=jedisPool.getResource();
+		String result=jedis.get(key);
+		jedis.close();
+		return result;
+	}
+
+	@Override
+	public String set(String key1, String value) {
+		Jedis jedis=jedisPool.getResource();
+		String result=jedis.set(key1,value);
+		jedis.close();
+		return result;
+	}
+
+	@Override
+	public long hset(String hkey, String key, String value) {
+		Jedis jedis=jedisPool.getResource();
+		long result=jedis.hset(hkey,key,value);
+		jedis.close();
+		return result;
+	}
+
+	@Override
+	public String hget(String hkey, String key) {
+		Jedis jedis=jedisPool.getResource();
+		String result=jedis.hget(hkey,key);
+		jedis.close();
+		return result;
+	}
+
+	@Override
+	public long incr(String key) {
+		Jedis jedis=jedisPool.getResource();
+		long result=jedis.incr(key);
+		jedis.close();
+		return result;
+	}
+
+	@Override
+	public long expire(String key,int seconds) {
+		Jedis jedis=jedisPool.getResource();
+		long result=jedis.expire(key, seconds);
+		jedis.close();
+		return result;
+	}
+
+	@Override
+	public long ttl(String key) {
+		Jedis jedis=jedisPool.getResource();
+		long result=jedis.ttl(key);
+		jedis.close();
+		return result;
+	}
+	
+	@Override
+	public long hdel(String hkey,String key) {
+		Jedis jedis=jedisPool.getResource();
+		long result=jedis.hdel(hkey, key);
+		jedis.close();
+		return result;
+	}
+
+}
